db1799657861c4bba58fe6423e6dc49e0a722d41
Dell <trixzen666@gmail.com>
2025-01-17 11:45:54 +0800

    good version 1.3

--- src/main/resources/static/js/contact.js
+++ src/main/resources/static/js/contact.js
@@ -18,17 +18,9 @@ async function addContact(event) {
     const name = document.getElementById('name').value.trim();
     const number = document.getElementById('number').value.trim();
 
-    // Нэр зөвхөн үсэг агуулж байгааг шалгах
-    if (!/^[a-zA-Zа-яА-ЯёЁ]+$/.test(name)) {
-        alert("Нэр зөвхөн үсэг агуулсан байх ёстой!");
+    if(!validateForm(name, number)) {
         return;
-    }
-
-    // Дугаар зөвхөн 8 оронтой тоо байхыг шалгах
-    if (!/^\d{8}$/.test(number)) {
-        alert("Дугаар заавал 8 оронтой байх ёстой!");
-        return;
-    }
+     }
 
     const formData = { name, number };
 
@@ -43,34 +35,19 @@ async function addContact(event) {
             document.getElementById('contactForm').reset();
         }
     } catch (error) {
-        console.error('Error:', error);
+        console.error('Алдаа:', error);
     }
 }
-
 
 // Load all contacts
 async function loadContacts() {
     try {
         const response = await fetch('/api/contact/');
-        const contacts = await response.json();
-        const tbody = document.querySelector('#contactsTable tbody');
-        tbody.innerHTML = '';
 
-        contacts.forEach((contact, index) => {
-            const tr = document.createElement('tr');
-            tr.innerHTML = `
-                <td><input type="checkbox" class="contact-checkbox" data-name="${contact.name}" data-number="${contact.number}"></td>
-                <td>${index + 1}</td>
-                <td contenteditable="true">${contact.name}</td>
-                <td contenteditable="true">${contact.number}</td>
-                <td>
-                    <button onclick="deleteContact('${contact.name}', '${contact.number}')" class="btn btn-sm btn-danger">Delete</button>
-                </td>
-            `;
-            tbody.appendChild(tr);
-        });
+        renderContacts(response);
+
     } catch (error) {
-        console.error('Error:', error);
+        console.error('Алдаа:', error);
     }
 }
 
@@ -80,13 +57,15 @@ async function saveContacts() {
      const selectedCheckboxes = document.querySelectorAll('.contact-checkbox:checked');
 
     if (selectedCheckboxes.length === 0) {
-        alert('Please select at least one row to save.');
+        alert('Хадгалахын тулд дор хаяж нэг мөр сонгоно уу.');
         return;
    }
   // Сонгогдсон бүх мөрний шинэ утгуудыг авах
     const contactsToSave = [];
-    selectedCheckboxes.forEach(checkbox => {
 
+    // Бүх чекбоксуудыг шалгах
+    for (let i = 0; i < selectedCheckboxes.length; i++) {
+        const checkbox = selectedCheckboxes[i];
         const row = checkbox.closest('tr'); // Сонгосон мөрийг авах
 
         const oldName = checkbox.getAttribute('data-name');
@@ -94,8 +73,14 @@ async function saveContacts() {
         const newName = row.children[2].textContent.trim();
         const newNumber = row.children[3].textContent.trim();
 
+        // Нэр, дугаарын шалгалтыг хийх
+        if (!validateForm(newName, newNumber)) {
+            return; // Алдаа гарсан бол процесс зогсоно
+        }
+
+        // Шалгалт амжилттай бол шинэ мэдээллийг нэмнэ
         contactsToSave.push({ oldName, oldNumber, newName, newNumber });
-    });
+    }
 
     try {
         const response = await fetch('/api/contact/saveSelected', {
@@ -105,14 +90,106 @@ async function saveContacts() {
         });
 
         if (response.ok) {
-            alert('Contact saved successfully!');
+            alert('Харилцагчийг амжилттай хадгаллаа!');
             loadContacts();
         } else {
             const error = await response.text();
-            alert(`Failed to save contact: ${error}`);
+            alert(`Харилцагчийг хадгалахад алдаа гарлаа!: ${error}`);
         }
     } catch (error) {
-        console.error('Error:', error);
+        console.error('Алдаа:', error);
+    }
+}
+
+// Delete contact
+async function deleteContact(name, number) {
+    if (!confirm('Та энэ харилцагчийг устгахдаа итгэлтэй байна уу?')) return;
+
+    try {
+        const response = await fetch(`/api/contact/delete?name=${name}&number=${number}`, {
+            method: 'DELETE'
+        });
+        if (response.ok) {
+            loadContacts();
+        }
+    } catch (error) {
+        console.error('Алдаа:', error);
+    }
+}
+
+// Search contacts
+async function searchContacts() {
+    const searchData = document.getElementById('searchData').value.trim();
+
+    // Хайлт хоосон байвал үйлдлийг зогсооно
+    if (!searchData) {
+        alert("Нэр эсвэл дугаар оруулна уу!");
+        return;
+    }
+
+    if (!validateSearch(searchData)) {
+        return
+    }
+
+    try {
+        const response = await fetch(`/api/contact/search?searchData=${searchData}`);
+
+        renderContacts(response);
+
+        if (response.ok) {
+            document.getElementById('searchData').value = '';
+        }
+
+    } catch (error) {
+        console.error('Алдаа:', error);
+    }
+}
+
+// Select or deselect all checkboxes
+function toggleSelectAll() {
+    const selectAllCheckbox = document.getElementById('selectAllCheckbox');
+    const checkboxes = document.querySelectorAll('.contact-checkbox');
+    checkboxes.forEach(checkbox => {
+        checkbox.checked = selectAllCheckbox.checked;
+    });
+}
+
+// Delete selected contacts
+async function deleteSelectedContacts() {
+    const selectedCheckboxes = document.querySelectorAll('.contact-checkbox:checked');
+
+    if (selectedCheckboxes.length === 0) {
+        alert('Устгах харилцагчдыг сонгоно уу!');
+        return;
+    }
+
+    if (!confirm('Та сонгосон харилцагчдыг устгахдаа итгэлтэй байна уу?')) return;
+
+    const contactsToDelete = [];
+    selectedCheckboxes.forEach(checkbox => {
+        contactsToDelete.push({
+            name: checkbox.getAttribute('data-name'),
+            number: checkbox.getAttribute('data-number')
+        });
+    });
+
+    try {
+        const response = await fetch('/api/contact/deleteSelected', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(contactsToDelete)
+        });
+
+        if (response.ok) {
+            loadContacts();
+        } else {
+            const error = await response.text();
+            console.error('Серверийн Алдаа:', error);
+            alert(`Failed to delete contacts: ${error}`);
+
+        }
+    } catch (error) {
+        console.error('Алдаа:', error);
     }
 }
 
@@ -137,99 +214,3 @@ async function saveContact(button, oldName, oldNumber) {
     }
 }
 */
-
-// Delete contact
-async function deleteContact(name, number) {
-    if (!confirm('Are you sure you want to delete this contact?')) return;
-
-    try {
-        const response = await fetch(`/api/contact/delete?name=${name}&number=${number}`, {
-            method: 'DELETE'
-        });
-        if (response.ok) {
-            loadContacts();
-        }
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
-
-// Search contacts
-async function searchContacts() {
-    const searchData = document.getElementById('searchData').value.trim();
-
-    try {
-        const response = await fetch(`/api/contact/search?searchData=${searchData}`);
-        const contacts = await response.json();
-        const tbody = document.querySelector('#contactsTable tbody');
-        tbody.innerHTML = '';
-
-        contacts.forEach((contact, index) => {
-            const tr = document.createElement('tr');
-            tr.innerHTML = `
-                <td><input type="checkbox" class="contact-checkbox" data-name="${contact.name}" data-number="${contact.number}"></td>
-                <td>${index + 1}</td>
-                <td contenteditable="true">${contact.name}</td>
-                <td contenteditable="true">${contact.number}</td>
-                <td>
-                    <button onclick="deleteContact('${contact.name}', '${contact.number}')" class="btn btn-sm btn-danger">Delete</button>
-                </td>
-            `;
-            tbody.appendChild(tr);
-        });
-        if (response.ok) {
-             document.getElementById('searchData').value = '';
-
-        }
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
-
-// Select or deselect all checkboxes
-function toggleSelectAll() {
-    const selectAllCheckbox = document.getElementById('selectAllCheckbox');
-    const checkboxes = document.querySelectorAll('.contact-checkbox');
-    checkboxes.forEach(checkbox => {
-        checkbox.checked = selectAllCheckbox.checked;
-    });
-}
-
-// Delete selected contacts
-async function deleteSelectedContacts() {
-    const selectedCheckboxes = document.querySelectorAll('.contact-checkbox:checked');
-
-    if (selectedCheckboxes.length === 0) {
-        alert('Please select contacts to delete.');
-        return;
-    }
-
-    if (!confirm('Are you sure you want to delete selected contacts?')) return;
-
-    const contactsToDelete = [];
-    selectedCheckboxes.forEach(checkbox => {
-        contactsToDelete.push({
-            name: checkbox.getAttribute('data-name'),
-            number: checkbox.getAttribute('data-number')
-        });
-    });
-
-    try {
-        const response = await fetch('/api/contact/deleteSelected', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(contactsToDelete)
-        });
-
-        if (response.ok) {
-            loadContacts();
-        } else {
-            const error = await response.text();
-            console.error('Server error:', error);
-            alert(`Failed to delete contacts: ${error}`);
-
-        }
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
